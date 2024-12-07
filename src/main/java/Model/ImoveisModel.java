647633272c1b946516a542be6f2b91aea16e4b88
Felipe-Santos-01 <feliperibeiro1569@gmail.com>
2024-12-07 17:06:12 -0300

    Teste 2
    Aqui atualizei a minha classe imoveis.

--- src/main/java/Model/ImoveisModel.java
+++ src/main/java/Model/ImoveisModel.java
@@ -1,5 +1,25 @@
 package Model;
 
+import lombok.Data;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="Imoveis")
+
 public class ImoveisModel {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    @Column(nullable = false, length = 100)
+    private String endereco;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @OneToMany(mappedBy="professor")
+    private List<DisciplinaModel> disciplinas;
+} 
