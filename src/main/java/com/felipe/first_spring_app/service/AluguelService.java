5635b13131b6fff092a198e9f9b12e3b7a34bc12
Ryan <ryan.3feitosa@gmail.com>
2024-12-16 08:06:25 -0300

    Adicionei o Service do AluguelService

--- src/main/java/com/felipe/first_spring_app/service/AluguelService.java
+++ src/main/java/com/felipe/first_spring_app/service/AluguelService.java
@@ -1,4 +1,49 @@
 package com.felipe.first_spring_app.service;
 
+
+import com.felipe.first_spring_app.model.AluguelModel;
+import com.felipe.first_spring_app.repositorio.AluguelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AluguelService {
+
+
+    @Autowired
+    private AluguelRepository aluguelRepository;
+
+
+    public AluguelService(AluguelRepository aluguelRepository) {
+        this.aluguelRepository = aluguelRepository;
+    }
+
+
+    public List<AluguelModel> listarTodos() {
+        return aluguelRepository.findAll();
+    }
+
+
+    public Optional<AluguelModel> buscarPorId(Long id) {
+        return aluguelRepository.findById(id);
+    }
+
+
+    public AluguelModel salvar(AluguelModel aluguel) {
+        return aluguelRepository.save(aluguel);
+    }
+
+
+    public static AluguelModel update(Long id, AluguelModel aluguel) {
+        return aluguel;
+    }
+
+
+    public void deletar(Long id) {
+        aluguelRepository.deleteById(id);
+    }
 }
+
